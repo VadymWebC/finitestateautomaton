1727988f055819bc157fbe76771ecabcce911878
VadymWebC <vadymcdevelweb@gmail.com>
2025-01-21 12:20:16 +0200

    building a simple finite state machine

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// IT'S A START
+;(() => {
+	var theIn = [..."101"]
+
+	var theState = {
+		0: {
+			0: {
+				0: {
+					res: 0,
+				},
+				1: {
+					res: 1,
+				},
+			},
+			1: {
+				0: {
+					res: 2,
+				},
+				1: {
+					res: 3,
+				},
+			},
+		},
+		1: {
+			0: {
+				0: {
+					res: 4,
+				},
+				1: {
+					res: 5,
+				},
+			},
+			1: {
+				0: {
+					res: 6,
+				},
+				1: {
+					res: 7,
+				},
+			},
+		},
+	}
+
+	const res = theIn.reduce(
+		(theState, theCommand) => theState[theCommand],
+		theState
+	)
+
+	console.log(res)
+})()
