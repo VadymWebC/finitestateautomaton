0bb9b9729350b004d164b822c47ab49d75341bf3
VadymWebC <vadymcdevelweb@gmail.com>
2025-01-22 18:22:56 +0200

    begin of the development of the enhanced morse decoder

---
+++ morse_version_2.js
@@ -0,0 +1,56 @@
+;(() => {
+	var theMorseBase = {
+		".-": "A",
+		"-...": "B",
+		"-.-.": "C",
+		"-..": "D",
+		".": "E",
+		"..-.": "F",
+		"--.": "G",
+		"....": "H",
+		"..": "I",
+		".---": "J",
+		"-.-": "K",
+		".-..": "L",
+		"--": "M",
+		"-.": "N",
+		"---": "O",
+		".--.": "P",
+		"--.-": "Q",
+		".-.": "R",
+		"...": "S",
+		"-": "T",
+		"..-": "U",
+		"...-": "V",
+		".--": "W",
+		"-..-": "X",
+		"-.--": "Y",
+		"--..": "Z",
+		"-----": "0",
+		".----": "1",
+		"..---": "2",
+		"...--": "3",
+		"....-": "4",
+		".....": "5",
+		"-....": "6",
+		"--...": "7",
+		"---..": "8",
+		"----.": "9",
+		"": " ",
+	}
+	var doBinaryParse = theIn => {
+		var theProgram = [...theIn]
+		var theState = {
+			".": theThing => theThing << 1,
+			"-": theThing => (theThing << 1) | 1,
+			" ": theThing => theThing,
+			other: () => {},
+		}
+		return theProgram.reduce(
+			(theRes, theCommand) => theState[theCommand](theRes),
+			1
+		)
+	}
+	var theIn = "....."
+	console.log(`Parsed: ${doBinaryParse(theIn)}`)
+})()
